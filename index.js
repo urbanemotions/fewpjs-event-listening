074d76c231fe17ce99e747c02bc71b5777477f46
sin wan wildy <sw.wildy@gmail.com>
2021-02-22 22:24:12 -0800

    commit tree 42715570f18c1818ea8db562299018559c3e5d43

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+function addingEventListener() 
+{
+    const input = document.getElementById('input');
+    input.addEventListener('click', function(event) 
+    {
+        alert('I was clicked!');
+    });
 }
